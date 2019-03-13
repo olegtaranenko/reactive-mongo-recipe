53d6fb08f00649b5f22fb5466678eaf3f2dec203
Oleg Taranenko <olegtaranenko@gmail.com>
2019-03-14 00:05:33 +0100

    generated by intellij/spring.io script

---
+++ src/main/java/com/olegtaranenko/udemy/reactivemongorecipe/ReactiveMongoRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.olegtaranenko.udemy.reactivemongorecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMongoRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olegtaranenko/udemy/reactivemongorecipe/ReactiveMongoRecipeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olegtaranenko.udemy.reactivemongorecipe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveMongoRecipeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
